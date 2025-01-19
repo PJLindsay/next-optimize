f66936b321da5419fe33e0a5a1d136caddf37866
= <=>
2025-01-19 12:23:02 -0700

    Add dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,21 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// static metadata
+// export const metadata = {
+//   title: "Browse all our X posts.",
+//   description: "Browse all our posts.",
+// };
+
+// dynamic metadata example
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all ${numberOfPosts} posts.`,
+    description: "Browse all our posts.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
